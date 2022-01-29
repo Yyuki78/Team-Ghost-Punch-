e51b7e1424cf51ef8b80ac0290118a59e210dd0b
Yyuki78 <yuki.yamada2411@gmail.com>
2022-01-29 14:32:52 +0900

    1st EnemyMove

---
+++ Assets/Script/Ghost/EnemyMoveTest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMoveTest : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>(); // NavMeshAgentを保持しておく
+    }
+
+    // CollisionDetectorのonTriggerStayにセットし、衝突判定を受け取るメソッド
+    public void OnDetectObject(Collider collider)
+    {
+        // 検知したオブジェクトに「Player」のタグがついていれば、そのオブジェクトを追いかける
+        if (collider.CompareTag("Player"))
+        {
+            _agent.destination = collider.transform.position;
+        }
+    }
+}
---
+++ Assets/Script/Ghost/EnemyStatus.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// 敵の状態管理スクリプト
+/// </summary>
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyStatus : MobStatus
+{
+    private NavMeshAgent _agent;
+    Vector3 _linkEndPos;
+    private bool RunEnable = true;
+    private bool once = false;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        Debug.Log(_state);
+        // NavMeshAgentのvelocityで移動速度のベクトルが取得できる
+        _animator.SetFloat("MoveSpeed", _agent.velocity.magnitude);
+    }
+
+    protected override void OnDie()
+    {
+        base.OnDie();
+        StartCoroutine(DestroyCoroutine());
+    }
+
+    public void Run()
+    {
+        if (!base.IsRunnable) return;
+        if (RunEnable == false) return;
+        Debug.Log("追いかけます");
+        _animator.SetBool("Run", true);
+        once = true;
+        base.GoToRunStateIfPossible();
+    }
+
+    //後でRunからNormalに戻る処理を追加する
+    /*
+    public void StandUp()
+    {
+        Debug.Log("立ち上がります");
+        SitEnable = false;
+        if (once == true)
+        {
+            once = false;
+            base.GoToNormalStateIfPossible();
+        }
+        _animator.SetBool("Sit", false);
+        StartCoroutine(SitEnableCoroutine());
+    }*/
+
+    /// <summary>
+    /// 倒された時の消滅コルーチンです。
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator DestroyCoroutine()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
+
+    private IEnumerator RunEnableCoroutine()
+    {
+        yield return new WaitForSeconds(2);
+        RunEnable = true;
+        yield break;
+    }
+}
