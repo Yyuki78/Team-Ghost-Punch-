9dbc9d532e22ecb1537e99f796768d73a5a1cd8f
YokoyamaTakanori <t.yoko303@gmail.com>
2022-01-29 22:01:12 +0900

    プレイヤーアニメーターを追加

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -6,14 +6,20 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     float m_moveSpeed = 1.0f;
+    [SerializeField]
+    bool m_isChargeMode = false;
 
 
     Rigidbody m_rigidbody = null;
+    Animator m_animator = null;
+    SpriteRenderer m_spriteRenderer = null;
 
     // Start is called before the first frame update
     void Awake()
     {
         m_rigidbody = GetComponent<Rigidbody>();
+        m_animator = GetComponent<Animator>();
+        m_spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -29,5 +35,21 @@ public class Player : MonoBehaviour
         var velocity = input * m_moveSpeed;
 
         m_rigidbody.velocity = velocity;
+        if(m_spriteRenderer != null && Mathf.Abs(x) > 0.2f) {
+            if(x > 0.0f) {
+                m_spriteRenderer.flipX = true;
+            } else {
+                m_spriteRenderer.flipX = false;
+            }
+        }
+    }
+
+    void updateCharge() {
+        var isCharging = Input.GetButton("Charge");
+        if(isCharging) {
+            m_animator.SetBool("Charge", true);
+        } else {
+            m_animator.SetBool("Charge", false);
+        }
     }
 }
