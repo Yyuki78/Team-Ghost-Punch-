14703fb984d65541b6d7c6df2c280b1f855b0561
Yyuki78 <yuki.yamada2411@gmail.com>
2022-01-30 09:58:26 +0900

    Enemy RigidBody Delete

--- Assets/Script/Ghost/EnemyMove.cs
+++ Assets/Script/Ghost/EnemyMove.cs
@@ -8,15 +8,50 @@ using UnityEngine.AI;
 public class EnemyMove : MonoBehaviour
 {
     [SerializeField] private LayerMask raycastLayerMask; // レイヤーマスク
+    const string LayerName1 = "Default";
+    const string LayerName2 = "Nothing";
+    private float step_time;
+    private int mask1;
+    private int mask2;
+    const int LayerNum = 8;
 
     private NavMeshAgent _agent;
     private RaycastHit[] _raycastHits = new RaycastHit[10];
     private EnemyStatus _status;
 
-    private void Start()
+    public void Start()
     {
+        mask1 = LayerMask.GetMask(new string[] { LayerName1 });
+        mask2 = LayerMask.GetMask(new string[] { LayerName2 });
+        step_time = 0.0f;
         _agent = GetComponent<NavMeshAgent>(); // NavMeshAgentを保持しておく
         _status = GetComponent<EnemyStatus>();
+    }
+
+    public void Update()
+    {
+        // 経過時間をカウント
+        step_time += Time.deltaTime;
+        if (Physics.Raycast(transform.position,
+                            transform.forward,
+                            float.PositiveInfinity,
+                            mask1))
+        {
+
+            Debug.Log("Raycast Hit!Mask1");
+        }
+        // 10秒後にMask2
+        if (step_time >= 10.0f)
+        {
+            if (Physics.Raycast(transform.position,
+                            transform.forward,
+                            float.PositiveInfinity,
+                            mask2))
+            {
+
+                Debug.Log("Raycast Hit!Mask2");
+            }
+        }
     }
 
     // CollisionDetectorのonTriggerStayにセットし、衝突判定を受け取るメソッド
