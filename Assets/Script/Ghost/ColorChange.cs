e179a26f42bd1e6170c6f6d58c9f7cdb8393f8e5
Yyuki78 <yuki.yamada2411@gmail.com>
2022-01-30 13:39:00 +0900

    Enemy ColorChange01

---
+++ Assets/Script/Ghost/ColorChange.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    private bool once = false;
+
+    [SerializeField] GameObject Player;
+    private Player _player;
+
+
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.material.color = sprite.material.color - new Color32(0, 0, 0, 0);
+        _player = Player.GetComponent<Player>();
+    }
+    void Update()
+    {
+        if (_player.IsDead == false && once == false)
+        {
+            once = true;
+            StartCoroutine("Transparent");
+            StopCoroutine("Trasparent2");
+
+        }
+        if (_player.IsDead == true && once == true)
+        {
+            StopCoroutine("Trasparent");
+            StartCoroutine("Transparent2");
+            once = false;
+        }
+    }
+
+    IEnumerator Transparent()
+    {
+        for (int i = 0; i < 200; i++)
+        {
+            sprite.material.color = sprite.material.color - new Color32(0, 0, 0, 1);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    IEnumerator Transparent2()
+    {
+        for (int i = 0; i < 200; i++)
+        {
+            sprite.material.color = sprite.material.color + new Color32(0, 0, 0, 1);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
